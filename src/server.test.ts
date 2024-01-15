6ae2793ce317e76538e237f10721cefbc7f16e3e
Max-Beier <max@maxbeier.dev>
2024-01-15 14:25:27 +0100

    test(DT-161) workflow test

--- src/server.test.ts
+++ src/server.test.ts
@@ -4,6 +4,7 @@ import { handler } from "./server.ts";
 Deno.test("GraphQL Connection", async () => {
     const server = Deno.serve({ port: 3000, handler })
     const response = await fetch("http://localhost:3000");
+    console.log(response.status)
     assertEquals(response.ok, true);
     await response.body?.cancel();
     await server.shutdown()
