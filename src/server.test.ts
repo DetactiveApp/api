4213b8058bf9e1e89445cd5a7f137c5f78cc8f73
Max-Beier <max@maxbeier.dev>
2024-01-15 14:23:58 +0100

    refactor(DT-161) test port

--- src/server.test.ts
+++ src/server.test.ts
@@ -2,8 +2,8 @@ import { assertEquals } from "https://deno.land/std@0.212.0/assert/mod.ts";
 import { handler } from "./server.ts";
 
 Deno.test("GraphQL Connection", async () => {
-    const server = Deno.serve({ port: 5000, handler })
-    const response = await fetch("http://localhost:5000");
+    const server = Deno.serve({ port: 3000, handler })
+    const response = await fetch("http://localhost:3000");
     assertEquals(response.ok, true);
     await response.body?.cancel();
     await server.shutdown()
