2492175c82a746d5e206de2aa8157139d3a28c73
Max Beier <max@maxbeier.dev>
2024-01-16 21:34:09 +0000

    feat(DT-161) added basic structure

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import yoga from "@elysiajs/graphql-yoga";
 import { Elysia } from "elysia";
-import { typeDefs } from "./typedef";
 import { resolvers } from "./resolvers";
+
+const api_version = require('../package.json').version;
 
 new Elysia()
     .use(
         yoga({
-            typeDefs: typeDefs,
+            typeDefs: await Bun.file("src/types/index.graphql").text(),
             resolvers: resolvers
         })
     )
+    .get("/", `Detactive API v${api_version}`)
     .listen(3000)
